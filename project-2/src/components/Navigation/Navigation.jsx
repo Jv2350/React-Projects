ae92869480e80eacd3a66a82534fd9f24a574f09
Jv2350 <tp8375735@gmail.com>
2024-06-06 22:55:23 +0530

    zzz

---
+++ project-2/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <nav className={`${styles.navigation} container`}>
+      <div className="logo">
+        <img src="/images/logo.png" alt="" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
