17f3e69b3a4d76afbc10479d882dd9426286a7f7
Jv2350 <tp8375735@gmail.com>
2024-06-22 23:46:33 +0530

    zzz

---
+++ project-5/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      gray: "#5a5959",
+      yellow: "#ffeaae",
+      "dark-yellow": "#fcca3f",
+      orange: "#f6820c",
+    },
+  },
+  plugins: [],
+};
