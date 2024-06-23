1b8ac2f0de97f2a4124564a80e098ee4903eddbd
Jv2350 <tp8375735@gmail.com>
2024-06-23 23:28:47 +0530

    zzz

--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -3,8 +3,14 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-[370px]">
       <Navbar />
+      <div className="flex">
+        <input
+          type="text"
+          className="h-10 flex-grow rounded-md border border-white bg-transparent"
+        />
+      </div>
     </div>
   );
 };
