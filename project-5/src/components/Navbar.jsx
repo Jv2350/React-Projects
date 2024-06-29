bd2a2b101ebd9980d7c3516524b8ff15943faeb2
Jv2350 <tp8375735@gmail.com>
2024-06-29 23:52:07 +0530

    zzz

--- project-5/src/components/Navbar.jsx
+++ project-5/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-[20px] font-bold">
       <img src="/firebase.svg" alt="" />
-      <h1>Firebase Contact App</h1>
+      <h1>Firebase Contact</h1>
     </div>
   );
 };
